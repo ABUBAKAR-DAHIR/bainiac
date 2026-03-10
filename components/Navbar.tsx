ace46515e319527db95a5ad1854d016553ee6037
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-03-10 13:36:11 +0530

    feat: initial commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,22 @@
 import Link from 'next/link'
-import React from 'react'
+import { Button } from './ui/button'
+import { Sun } from 'lucide-react'
 
 export default function Navbar() {
   return (
-    <nav className='max-w-7xl flex mx-auto bg-amber-700 justify-between items-center'>
-        <h1 className='flex-1/2'>bain <span>iac</span></h1>
+    <nav className='max-w-7xl flex mx-auto justify-between items-center my-4 border rounded-2xl px-10'>
+        <h1 className='w-1/2 capitalize font-bold text-xl tracking-wide'>bain<span className='bg-primary rounded-xl px-2 py-1'>iac</span></h1>
 
-        <ul className='flex justify-evenly flex-1/2 bg-red-700'>
-            <Link href="#" className='py-10'>about</Link>
-            <Link href="#" className='py-10'>subjects</Link>
-            <Link href="#" className='py-10'>dashboard</Link>
+        <ul className='w-1/3 flex capitalize items-center justify-center'>
+            <Link href="#" className='py-10 hover:bg-amber-400 w-full text-center'>about</Link>
+            <Link href="#" className='py-10 hover:bg-amber-400 w-full text-center'>subjects</Link>
+            <Link href="#" className='py-10 hover:bg-amber-400 w-full text-center'>contact</Link>
         </ul>
 
-        <Button>dfd</Button>
+        <div className="w-1/3 flex items-center justify-evenly">
+            <Button className=''>dashboard</Button>
+            <Button className=''><Sun /></Button>
+        </div>
 
 
     </nav>
